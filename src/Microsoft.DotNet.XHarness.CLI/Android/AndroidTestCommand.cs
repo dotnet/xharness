e1555e15718bca847974a75dbd4fd136bea25ac4
Premek Vysoky <premek.vysoky@microsoft.com>
2020-03-30 15:52:37 +0200

    Remove duplicate .sln and move projects up by a level

--- src/Microsoft.DotNet.XHarness.CLI/Android/AndroidTestCommand.cs
+++ src/Microsoft.DotNet.XHarness.CLI/Android/AndroidTestCommand.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.DotNet.XHarness.CLI.Common;
 using Mono.Options;
 using System;
 using System.Collections.Generic;
