dfc0b9a8ec3e7fdfa0062a7b429d29486048677f
Přemek Vysoký <premek.vysoky@microsoft.com>
2020-03-30 18:20:41 +0200

    Move the command line project to CLI namespace (#10)
    New structure:
    - Microsoft.DotNet.XHarness.CLI project - handles command line tool args and invokes implementation
    - Microsoft.DotNet.XHarness.Android/iOS projects - implementations
    - Microsoft.DotNet.XHarness.iOS.Shared - dependency that we we share with Xamarin
    
    Proposed new structure:
    ```
    .
    |-- src
    |   |-- Microsoft.DotNet.XHarness.Android
    |   |-- Microsoft.DotNet.XHarness.CLI
    |   |   |-- Android
    |   |   |   |-- AndroidCommandSet.cs
    |   |   |   |-- AndroidGetStateCommand.cs
    |   |   |   `-- AndroidTestCommand.cs
    |   |   |-- Common
    |   |   |   |-- TestCommand.cs
    |   |   |   `-- XHarnessHelpCommand.cs
    |   |   |-- Properties
    |   |   |   `-- launchSettings.json
    |   |   |-- iOS
    |   |   |   |-- iOSCommandSet.cs
    |   |   |   |-- iOSGetStateCommand.cs
    |   |   |   |-- iOSPackageCommand.cs
    |   |   |   `-- iOSTestCommand.cs
    |   |   |-- Microsoft.DotNet.XHarness.CLI.csproj
    |   |   `-- Program.cs
    |   |-- Microsoft.DotNet.XHarness.iOS
    |   `-- Microsoft.DotNet.XHarness.iOS.Shared
    |-- tests
    |   `-- XHarness.Tests
    |       |-- XHarness.Tests.cs
    |       `-- XHarness.Tests.csproj
    |-- Build.cmd
    |-- CODE_OF_CONDUCT.md
    |-- Directory.Build.props
    |-- Directory.Build.targets
    |-- LICENSE.TXT
    |-- NuGet.config
    |-- README.md
    |-- SECURITY.md
    |-- THIRD-PARTY-NOTICES.TXT
    |-- XHarness.sln
    |-- azure-pipelines.yml
    |-- build.sh
    `-- global.json
    ```

---
+++ src/Microsoft.DotNet.XHarness.CLI/iOS/iOSGetStateCommand.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.0
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Mono.Options;
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.DotNet.XHarness.CLI.iOS
+{
+    public class iOSGetStateCommand : Command
+    {
+        bool ShowHelp = false;
+
+        public iOSGetStateCommand() : base("state")
+        {
+            Options = new OptionSet() {
+                "usage: ios state",
+                "",
+                "Print information about the current machine, such as host machine info and device status",
+                { "help|h", "Show this message", v => ShowHelp = v != null }
+            };
+        }
+
+        public override int Invoke(IEnumerable<string> arguments)
+        {
+            // Deal with unknown options and print nicely 
+            var extra = Options.Parse(arguments);
+            if (ShowHelp)
+            {
+                Options.WriteOptionDescriptions(Console.Out);
+                return 1;
+            }
+            if (extra.Count > 0)
+            {
+                Console.WriteLine($"Unknown arguments: {string.Join(" ", extra)}");
+                Options.WriteOptionDescriptions(Console.Out);
+            }
+            Console.WriteLine("iOS state command called (no args supported)");
+            return 0;
+        }
+    }
+}
