7ff63a07d6179461647281f6a77dc845ed6ec2a3
Manuel de la Pena <mandel@microsoft.com>
2020-04-16 11:48:24 -0400

    [iOS] Migrate code from xamanrin/xamarin-macios to the repo. (#31)
    The following code use to live under
    https://github.com/xamarin/xamarin-macios but has moved here. The code
    is what allows to:
    
    * Create a facade applicaton for testing dlls.
    * Execute tests in simulator/devices.
    
    This code has been worked on for years and is been tested. There are a
    number of unit tests that are currently in xamarin-macios that will be
    moved in a different PR to ensure that the tests work correctly in the
    new xharness CI. There have been just two cosmetic changes:
    
    * Code formatting: Adapted to the dotnet core style.
    * OSS header added.

---
+++ src/Microsoft.DotNet.XHarness.iOS.Shared/AppBundleInformation.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.DotNet.XHarness.iOS.Shared
+{
+    public class AppBundleInformation
+    {
+        public string AppName { get; }
+        public string BundleIdentifier { get; }
+        public string AppPath { get; }
+        public string Variation { get; set; }
+        public string LaunchAppPath { get; }
+        public Extension? Extension { get; }
+
+        public AppBundleInformation(string appName, string bundleIdentifier, string appPath, string launchAppPath, Extension? extension)
+        {
+            AppName = appName;
+            BundleIdentifier = bundleIdentifier;
+            AppPath = appPath;
+            LaunchAppPath = launchAppPath;
+            Extension = extension;
+        }
+    }
+}
---
+++ src/Microsoft.DotNet.XHarness.iOS.Shared/Hardware/NoDeviceFoundException.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.Serialization;
+
+namespace Microsoft.DotNet.XHarness.iOS.Shared.Hardware
+{
+    public class NoDeviceFoundException : Exception
+    {
+        public NoDeviceFoundException()
+        {
+        }
+
+        public NoDeviceFoundException(string message) : base(message)
+        {
+        }
+
+        public NoDeviceFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NoDeviceFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
