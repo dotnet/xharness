a4e8d3a17797a746db382293c7d4f1c9bf6f25f3
Manuel de la Pena <mandel@microsoft.com>
2020-04-27 08:12:39 -0400

    [iOS] Do not generate the html report unless it is requested. (#61)
    Do not generate the hml per default. This is a feature for
    xamarin-macios and has to be explicitly enabled.
    
    fixes: #55
    
    Co-authored-by: Premek Vysoky <premek.vysoky@microsoft.com>

--- src/Microsoft.DotNet.XHarness.iOS.Shared/IResultParser.cs
+++ src/Microsoft.DotNet.XHarness.iOS.Shared/IResultParser.cs
@@ -8,33 +8,56 @@ using Microsoft.DotNet.XHarness.iOS.Shared.Logging;
 
 namespace Microsoft.DotNet.XHarness.iOS.Shared
 {
-
-    // Interface that represents an object that know how to parse results and generate timeout/crash/build errors so
-    // that CIs like VSTS and helix can parse them.
+    /// <summary>
+    /// Interface that represents an object that know how to parse results and generate timeout/crash/build errors so
+    /// that CIs like VSTS and helix can parse them.
+    /// </summary>
     public interface IResultParser
     {
-
-        // generates a xml result that will consider to be an error by the CI. Allows to catch errors in cases in which we are not talking about a test
-        // failure perse but the situation in which the app could not be built, timeout or crashed.
+        /// <summary>
+        /// Generates an XML result that will consider to be an error by the CI. Allows to catch errors in cases in which we are not talking about a test
+        /// failure perse but the situation in which the app could not be built, timeout or crashed.
+        /// </summary>
         void GenerateFailure(ILogs logs, string source, string appName, string variation, string title, string message, string stderrPath, XmlResultJargon jargon);
 
-        // updates a given xml result to contain a list of attachments. This is useful for CI to be able to add logs as part of the attachments of a failing test.
+        /// <summary>
+        /// Updates given xml result to contain a list of attachments. This is useful for CI to be able to add logs as part of the attachments of a failing test.
+        /// </summary>
         void UpdateMissingData(string source, string destination, string applicationName, IEnumerable<string> attachments);
 
-        // ensures that the given path contains a valid xml result and set the type of xml jargon found in the file.
+        /// <summary>
+        /// Ensures that the given path contains a valid xml result and set the type of xml jargon found in the file.
+        /// </summary>
         bool IsValidXml(string path, out XmlResultJargon type);
 
-        // takes a xml file and removes any extra data that makes the test result not to be a pure xml result for the given jargon.
+        /// <summary>
+        /// Takes an XML file and removes any extra data that makes the test result not to be a pure xml result for the given jargon.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
         void CleanXml(string source, string destination);
 
-        // Returns the path to be used for the given jargon.
+        /// <summary>
+        /// Returns the path to be used for the given jargon.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="xmlType"></param>
+        /// <returns></returns>
         string GetXmlFilePath(string path, XmlResultJargon xmlType);
 
-        // parses the xml of the given jargon, create a human readable result and returns a result line with the summary of what was
-        // parsed.
-        (string resultLine, bool failed) GenerateHumanReadableResults(string source, string destination, XmlResultJargon xmlType);
+        /// <summary>
+        /// Parses the xml of the given jargon and returns a result line with the summary of what was parsed.
+        /// If destination is provided, creates a human readable report.
+        /// </summary>
+        /// <param name="source">File that will be read</param>
+        /// <param name="xmlType">Jargon of the source file</param>
+        /// <param name="humanReadableReportDestination">If provided, will contain human readable result</param>
+        /// <returns>A result line with the summary of what was parsed and an over all result</returns>
+        (string resultLine, bool failed) ParseResults(string source, XmlResultJargon xmlType, string humanReadableReportDestination = null);
 
-        // generated a human readable test report.
+        /// <summary>
+        /// Generates a human readable test report.
+        /// </summary>
         void GenerateTestReport(StreamWriter writer, string resultsPath, XmlResultJargon xmlType);
     }
 }
