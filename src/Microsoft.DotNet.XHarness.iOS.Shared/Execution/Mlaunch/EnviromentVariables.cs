4e8c924ba202ccb7f30baff4dc16e0ee702b9904
Manuel de la Pena <mandel@microsoft.com>
2020-04-28 08:59:33 -0400

    Make variables const.

--- src/Microsoft.DotNet.XHarness.iOS.Shared/Execution/Mlaunch/EnviromentVariables.cs
+++ src/Microsoft.DotNet.XHarness.iOS.Shared/Execution/Mlaunch/EnviromentVariables.cs
@@ -6,68 +6,68 @@ namespace Microsoft.DotNet.XHarness.iOS.Shared.Execution.Mlaunch
         /// <summary>
         /// Env var that will tell the runner to start the execution of the tests automatically.
         /// </summary>
-        public static string AutoStart = "NUNIT_AUTOSTART";
+        public const string AutoStart = "NUNIT_AUTOSTART";
 
         /// <summary>
         /// Env car that will tell the test application to exit once all the test have been ran.
         /// </summary>
-        public static string AutoExit = "NUNIT_AUTOEXIT";
+        public const string AutoExit = "NUNIT_AUTOEXIT";
 
         /// <summary>
         /// Env var that will tell the test application to enable network on the device (iOS).
         /// </summary>
-        public static string EnableNetwork = "NUNIT_ENABLE_NETWORK";
+        public const string EnableNetwork = "NUNIT_ENABLE_NETWORK";
 
         /// <summary>
         /// Env var that will tell the test application to ignore those tests that required a permission to
         /// execute on the device.
         /// </summary>
-        public static string DisableSystemPermissionTests = "DISABLE_SYSTEM_PERMISSION_TESTS";
+        public const string DisableSystemPermissionTests = "DISABLE_SYSTEM_PERMISSION_TESTS";
 
         /// <summary>
         /// Env var that provide the test application the name of the host.
         /// </summary>
-        public static string HostName = "NUNIT_HOSTNAME";
+        public const string HostName = "NUNIT_HOSTNAME";
 
         /// <summary>
         /// Env var that provides the test application with the transport to use to communicate with the host.
         /// </summary>
-        public static string Transport = "NUNIT_TRANSPORT";
+        public const string Transport = "NUNIT_TRANSPORT";
 
         /// <summary>
         /// Env var that provides the test application with the path to be used to store the execution logs.
         /// </summary>
-        public static string LogFilePath = "NUNIT_LOG_FILE";
+        public const string LogFilePath = "NUNIT_LOG_FILE";
 
         /// <summary>
         /// Env var that provide the test application the port to be used to connect with the host.
         /// </summary>
-        public static string HostPort = "NUNIT_HOSTPORT";
+        public const string HostPort = "NUNIT_HOSTPORT";
 
         /// <summary>
         /// Env var used to notify the test application that the communication will be done using a tcp tunnel
         /// over the usb cable.
         /// </summary>
-        public static string UseTcpTunnel = "USE_TCP_TUNNEL";
+        public const string UseTcpTunnel = "USE_TCP_TUNNEL";
 
         /// <summary>
         /// Env var used to notify the test application that the output is expected to be xml.
         /// </summary>
-        public static string EnableXmlOutput = "NUNIT_ENABLE_XML_OUTPUT";
+        public const string EnableXmlOutput = "NUNIT_ENABLE_XML_OUTPUT";
 
         /// <summary>
         /// Env var used to notify the test application the xml mode to be used.
         /// </summary>
-        public static string XmlMode = "NUNIT_ENABLE_XML_MODE";
+        public const string XmlMode = "NUNIT_ENABLE_XML_MODE";
 
         /// <summary>
         /// Env var used to pass the format of the xml used for results.
         /// </summary>
-        public static string XmlVersion = "NUNIT_XML_VERSION";
+        public const string XmlVersion = "NUNIT_XML_VERSION";
 
         /// <summary>
         /// Env var used to notify the test application that the test should be sorted by name.
         /// </summary>
-        public static string SortByName = "NUNIT_SORTNAMES";
+        public const string SortByName = "NUNIT_SORTNAMES";
     }
 }
