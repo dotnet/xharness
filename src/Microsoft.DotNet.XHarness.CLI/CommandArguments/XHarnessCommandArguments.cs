4be256c6c3f01055a6e0070ad0860ff8060b1823
Premek Vysoky <premek.vysoky@microsoft.com>
2020-04-29 18:54:22 +0200

    Add method for parsing enums

--- src/Microsoft.DotNet.XHarness.CLI/CommandArguments/XHarnessCommandArguments.cs
+++ src/Microsoft.DotNet.XHarness.CLI/CommandArguments/XHarnessCommandArguments.cs
@@ -2,15 +2,32 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Microsoft.Extensions.Logging;
+using Mono.Options;
 
 namespace Microsoft.DotNet.XHarness.CLI.CommandArguments
 {
     internal abstract class XHarnessCommandArguments : ICommandArguments
     {
         public LogLevel Verbosity { get; set; }
+        public bool ShowHelp { get; set; }
+
+        protected virtual OptionSet GetOptions() => new OptionSet
+        {
+            {
+                "verbosity=|v=",
+                "Verbosity level (1-6) where higher means less logging. (default = 2 / Information)",
+                v =>
+                {
+                    Verbosity = ParseArgument<LogLevel>("verbosity", v);
+                }
+            },
+            { "help|h", "Show this message", v => ShowHelp = v != null }
+        };
 
         public abstract IList<string> GetValidationErrors();
 
@@ -22,6 +39,33 @@ namespace Microsoft.DotNet.XHarness.CLI.CommandArguments
             }
 
             return path;
+        }
+
+        protected static TEnum ParseArgument<TEnum>(string argumentName, string? value, params TEnum[]? invalidValues) where TEnum : struct, IConvertible
+        {
+            var type = typeof(TEnum);
+
+            if (!type.IsEnum)
+            {
+                throw new ArgumentException(nameof(TEnum) + " must be an enumerated type");
+            }
+
+            if (Enum.TryParse(value, ignoreCase: true, out TEnum result))
+            {
+                return result;
+            }
+
+            IEnumerable<TEnum> validOptions = Enum.GetValues(type).Cast<TEnum>();
+
+            if (invalidValues != null)
+            {
+                validOptions = validOptions.Where(v => !invalidValues.Contains(v));
+            }
+
+            throw new ArgumentException(
+                $"Invalid value '{value}' supplied in {argumentName}. " +
+                $"Valid values are:{Environment.NewLine}" +
+                $"\t- {string.Join($"{Environment.NewLine}\t- ", validOptions.Select(t => t.ToString()))}");
         }
     }
 }
