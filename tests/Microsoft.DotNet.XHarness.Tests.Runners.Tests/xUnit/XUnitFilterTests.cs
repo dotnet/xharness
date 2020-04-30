8c75183205a8ac399f3f0c516032a25045645917
Premek Vysoky <premek.vysoky@microsoft.com>
2020-04-30 10:52:27 +0200

    Make the Runners.Tests project a friendly assembly

--- tests/Microsoft.DotNet.XHarness.Tests.Runners.Tests/xUnit/XUnitFilterTests.cs
+++ tests/Microsoft.DotNet.XHarness.Tests.Runners.Tests/xUnit/XUnitFilterTests.cs
@@ -2,9 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 using System;
-using System.Runtime.InteropServices;
-using Xunit;
 using Microsoft.DotNet.XHarness.Tests.Runners.Xunit;
+using Xunit;
 
 namespace Microsoft.DotNet.XHarness.Tests.Runners.Tests
 {
