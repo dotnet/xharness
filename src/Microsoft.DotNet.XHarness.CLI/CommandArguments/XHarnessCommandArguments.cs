3893b6ace0f501d4776bc92adf2891d29811ebef
Premek Vysoky <premek.vysoky@microsoft.com>
2020-05-06 19:19:17 +0200

    Allow None

--- src/Microsoft.DotNet.XHarness.CLI/CommandArguments/XHarnessCommandArguments.cs
+++ src/Microsoft.DotNet.XHarness.CLI/CommandArguments/XHarnessCommandArguments.cs
@@ -29,7 +29,7 @@ namespace Microsoft.DotNet.XHarness.CLI.CommandArguments
             var options = GetCommandOptions();
 
             options.Add("verbosity:|v:", "Verbosity level - defaults to 'Information'. If passed without value, 'Debug' is assumed (highest)",
-                v => Verbosity = string.IsNullOrEmpty(v) ? LogLevel.Debug : ParseArgument("verbosity", v, invalidValues: LogLevel.None));
+                v => Verbosity = string.IsNullOrEmpty(v) ? LogLevel.Debug : ParseArgument("verbosity", v));
 
             options.Add("help|h", v => ShowHelp = v != null);
 
