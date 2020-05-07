c1f608d18b89f0631ce153f636e7587f6205a39f
Steve Pfister <steve.pfister@microsoft.com>
2020-05-07 13:53:17 -0400

    Adds support for skipping tests when SkipTestException is raised
    If a test fails and the exception type is SkipTestException, then the runner
    will flag that as a skipped test.  This is to support tests that depend on something in the xunit
    pipeline that may not be supported on mobile.  We don't want to have to go into every test and
    mark ConditionalFact/Theory.

--- src/Microsoft.DotNet.XHarness.Tests.Runners/xUnit/XUnitTestRunner.cs
+++ src/Microsoft.DotNet.XHarness.Tests.Runners/xUnit/XUnitTestRunner.cs
@@ -141,6 +141,8 @@ namespace Microsoft.DotNet.XHarness.Tests.Runners.Xunit
             if (args == null || args.Message == null)
                 return;
 
+            RaiseTestSkippedCase(args.Message, args.Message.TestCases, args.Message.TestCase);
+            /*
             SkippedTests++;
             OnInfo($"\t[IGNORED] {args.Message.TestCase.DisplayName}");
             LogTestDetails(args.Message.Test, log: OnDebug);
@@ -151,6 +153,7 @@ namespace Microsoft.DotNet.XHarness.Tests.Runners.Xunit
                 TestName: args.Message.Test.DisplayName,
                 TestResult: TestResult.Skipped
             ));
+            */
         }
 
         void HandleTestPassed(MessageHandlerArgs<ITestPassed> args)
@@ -228,6 +231,13 @@ namespace Microsoft.DotNet.XHarness.Tests.Runners.Xunit
         {
             if (args == null || args.Message == null)
                 return;
+
+            // For SkipTestExceptions, we treat as a skip instead of a failure
+            var exceptionType = args.Message.ExceptionTypes.FirstOrDefault();
+            if (exceptionType == "Microsoft.DotNet.XUnitExtensions.SkipTestException")
+            {
+                RaiseTestSkippedCase(args.Message, args.Message.TestCases, args.Message.TestCase);
+            }
 
             FailedTests++;
             string assemblyInfo = GetAssemblyInfo(args.Message.TestAssembly);
@@ -575,6 +585,20 @@ namespace Microsoft.DotNet.XHarness.Tests.Runners.Xunit
             ITestCase singleTestCase = args.Message.TestCase;
             ReportTestCases("Discovered", args.Message.TestCases, log: OnInfo, ignore: (ITestCase tc) => tc == singleTestCase);
             ReportTestCase("Discovered", singleTestCase, log: OnInfo);
+        }
+
+        void RaiseTestSkippedCase(ITestResultMessage message, IEnumerable<ITestCase> testCases, ITestCase testCase)
+        {
+            SkippedTests++;
+            OnInfo($"\t[IGNORED] {testCase.DisplayName}");
+            LogTestDetails(message.Test, log: OnDebug);
+            LogTestOutput(message, log: OnDiagnostic);
+            ReportTestCases("   Associated", testCases, log: OnDiagnostic);
+            // notify that the test completed because it was skipped
+            OnTestCompleted((
+                TestName: message.Test.DisplayName,
+                TestResult: TestResult.Skipped
+            ));
         }
 
         void ReportTestCases(string verb, IEnumerable<ITestCase> testCases, ITestCase ignoreTestCase, Action<string> log = null)
