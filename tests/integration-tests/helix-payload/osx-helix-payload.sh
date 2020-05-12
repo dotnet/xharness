5365f3db98459bc56b1faa2d73657269ab5b8956
Přemek Vysoký <premek.vysoky@microsoft.com>
2020-05-12 08:40:47 +0200

    [iOS] Add iOS integration tests build stage (#163)
    This adds a new stage that runs for public builds and tests our tool E2E on an iPhone Simulator.
    
    1. We publish the nupkg with the XHarness tool as an artifact (compiled in Debug mode)
    2. The integration tests stage sends a Helix job to the staging `OSX.1015.Amd64.iOS.Open` queue 
    3. The job downloads the nupkg, installs the dotnet tool locally and a gets a test app from Azure storage account, then runs it on a 64 bit Simulator
    
    There are few things where we could improve things but I think we need this asap to have some sort of a validation

---
+++ tests/integration-tests/helix-payload/osx-helix-payload.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -e
+
+version='1.0.0-ci'
+
+# Clean the NuGet cache from the previous 1.0.0-ci version of the tool
+# TODO: This might have a better solution: https://github.com/dotnet/xharness/issues/123
+echo "Cleaning the NuGet cache from the previous version of the tool..."
+
+# Call dotnet to get rid of the welcome message since the nuget command doesn't respect the --no-logo
+dotnet nuget locals All -l
+cache_dirs=`dotnet nuget locals All -l | cut -d':' -f 2 | tr -d ' '`
+while IFS= read -r path; do
+    echo "Purging cache in $path..."
+    rm -vrf "$path/microsoft.dotnet.xharness.cli/$version"
+    rm -vrf "$path/Microsoft.DotNet.XHarness.CLI/$version"
+done <<< "$cache_dirs"
+
+set -x
+
+here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# TODO - Package the app bundle here using `dotnet xharness ios package ..`
+# For now we download a pre-bundled app:
+curl -L --output $here/app.zip "https://netcorenativeassets.blob.core.windows.net/resource-packages/external/macos/test-ios-app/System.Numerics.Vectors.Tests.app.zip"
+app_name='System.Numerics.Vectors.Tests.app'
+
+tar -xzf app.zip
+
+mkdir $here/tools
+cd $here/tools
+
+dotnet new tool-manifest
+
+dotnet tool install --no-cache --version $version --add-source .. Microsoft.DotNet.XHarness.CLI
+
+export XHARNESS_DISABLE_COLORED_OUTPUT=true
+export XHARNESS_LOG_WITH_TIMESTAMPS=true
+
+# We have to call this otherwise mlaunch fails to spawn it properly
+open -a /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app
+
+dotnet tool restore --no-cache
+dotnet xharness ios test           \
+    --app="$here/$app_name"        \
+    --output-directory="$1"        \
+    --targets=ios-simulator-64     \
+    --timeout=600                  \
+    --launch-timeout=360           \
+    --communication-channel=Network
+
+set +e
+
+result=$?
+
+test_results=`ls $1/xunit-*.xml`
+
+if [ ! -f "$test_results" ]; then
+    echo "Failed to find xUnit tests results in the output directory. Existing files:"
+    ls -la $1
+    exit 1
+fi
+
+echo "Found test results in $1/$test_results. Renaming to testResults.xml"
+
+mv $test_results $1/testResults.xml
+
+if ! cat $1/testResults.xml | grep 'collection total="19" passed="19" failed="0" skipped="0"'; then
+    echo "Failed to detect result line"
+    exit 1
+fi
+
+exit $result
