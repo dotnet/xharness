94c08857084d7a40ad039969f51d9e558afead18
Přemek Vysoký <premek.vysoky@microsoft.com>
2020-05-13 17:58:30 +0200

    Bump SDK versions (iOS to 13.4) (#183)

--- src/Microsoft.DotNet.XHarness.iOS.Shared/SdkVersions.cs
+++ src/Microsoft.DotNet.XHarness.iOS.Shared/SdkVersions.cs
@@ -1,17 +1,13 @@
 ﻿namespace Microsoft.DotNet.XHarness.iOS.Shared {
-	/// <summary>
-	/// This file is auto-generated from xamarin-macios/Make.config:
-	/// https://github.com/xamarin/xamarin-macios/blob/54194e71f17dcbba76e6ca6b079f347f13f36dcf/Make.config
-	/// It is generated on build time and thus tied to the MacOS where it's being built.
-	/// 
-	/// TODO: We will need to start syncing this file or make it dynamic based on the host.
-	/// </summary>
+    /// <summary>
+    /// This file decides what environment will XHarness expect and what Simulators to be preinstalled.
+    /// </summary>
 	public static class SdkVersions {
-		public static string Xcode { get; private set; } = "11.3";
+		public static string Xcode { get; private set; } = "11.4";
 		public static string OSX { get; private set; } = "10.15";
-		public static string iOS { get; private set; } = "13.2";
-		public static string WatchOS { get; private set; } = "6.1";
-		public static string TVOS { get; private set; } = "13.2";
+		public static string iOS { get; private set; } = "13.4";
+		public static string WatchOS { get; private set; } = "6.2";
+		public static string TVOS { get; private set; } = "13.4";
 
 		public static string MinOSX { get; private set; } = "10.9";
 		public static string MiniOS { get; private set; } = "7.0";
@@ -23,14 +19,14 @@
 		public static string MinWatchOSCompanionSimulator { get; private set; } = "10.3";
 		public static string MinTVOSSimulator { get; private set; } = "10.2";
 
-		public static string MaxiOSSimulator { get; private set; } = "13.3";
-		public static string MaxWatchOSSimulator { get; private set; } = "6.1";
-		public static string MaxWatchOSCompanionSimulator { get; private set; } = "13.3";
+		public static string MaxiOSSimulator { get; private set; } = "13.4";
+		public static string MaxWatchOSSimulator { get; private set; } = "6.2";
+		public static string MaxWatchOSCompanionSimulator { get; private set; } = "13.4";
 
-		public static string MaxTVOSSimulator { get; private set; } = "13.3";
-		public static string MaxiOSDeploymentTarget { get; private set; } = "13.3";
-		public static string MaxWatchDeploymentTarget { get; private set; } = "6.1";
-		public static string MaxTVOSDeploymentTarget { get; private set; } = "13.3";
+		public static string MaxTVOSSimulator { get; private set; } = "13.4";
+		public static string MaxiOSDeploymentTarget { get; private set; } = "13.4";
+		public static string MaxWatchDeploymentTarget { get; private set; } = "6.2";
+		public static string MaxTVOSDeploymentTarget { get; private set; } = "13.4";
 
 		public static void OverrideVersions (string xcode,
 			string oSX,
