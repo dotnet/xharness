abe658f9bd9d4d2bdd624c314b574224281de9e8
Premek Vysoky <premek.vysoky@microsoft.com>
2020-05-18 12:30:50 +0200

    Add validation of the simulator tool

--- tests/integration-tests/helix-payloads/simulatorinstaller-integration-tests.sh
+++ tests/integration-tests/helix-payloads/simulatorinstaller-integration-tests.sh
@@ -20,6 +20,7 @@ done <<< "$cache_dirs"
 set -x
 
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $here
 
 mkdir $here/tools
 cd $here/tools
@@ -32,4 +33,78 @@ export XHARNESS_DISABLE_COLORED_OUTPUT=true
 export XHARNESS_LOG_WITH_TIMESTAMPS=true
 
 dotnet tool restore --no-cache
-dotnet simulator-installer list
+
+set +ex
+
+echo "Testing simulator download availability"
+echo "Getting list of available simulators"
+
+IFS=$'\n'
+list=($(dotnet simulator-installer list | grep 'Source:'))
+
+length="${#list[@]}"
+
+echo "Found $length simulators"
+echo ""
+
+if [ ! "$length" -gt 0 ]; then
+    echo "Couldn't list available simulators" 1>&2
+    exit 1
+fi
+
+result=0
+
+# Test 3 random URLs
+for (( i=0; i<3; i++ )); do
+    random_line=${list[$RANDOM % ${#list[@]}]}
+    url=`echo $random_line | tr -s ' ' | cut -d ' ' -f 3`
+    echo "Testing $url..."
+
+    status_code=`curl --silent --head -o /dev/null --write-out %{http_code} "$url"`
+
+    echo "Result: $status_code"
+
+    if [ "$status_code" == "200" ]; then
+        echo "Test succeeded"
+    else
+        echo "Failed loading URL"
+        result=1
+    fi
+
+    echo ""
+done
+
+echo "Testing installed simulators and the find command"
+
+IFS=$'\n'
+installed_simulators=($(dotnet simulator-installer list --installed | grep 'Identifier:'))
+
+length="${#installed_simulators[@]}"
+
+echo "Found $length installed simulators:"
+
+simulator_args=""
+
+for i in "${installed_simulators[@]}"
+do
+    pkg_name=`echo $i | tr -s ' ' | cut -d ' ' -f 3`
+    echo "  $pkg_name"
+    simulator_args="--simulator=$pkg_name $simulator_args"
+done
+
+echo ""
+set -x
+
+eval dotnet simulator-installer find $simulator_args
+
+if [ "$?" != 0 ]; then
+    echo "Failed to find listed simulators"
+    result=1
+else
+    echo "Found all listed simulators"
+fi
+
+cd $here
+rm -rf $here/tools
+
+exit $result
