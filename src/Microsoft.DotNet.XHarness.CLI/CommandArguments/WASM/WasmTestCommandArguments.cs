bf873c70b1f983f4c9c7280ccb0481142ef64206
Maxim Lipnin <v-maxlip@microsoft.com>
2020-06-01 13:39:39 +0300

    [wasm] Add running wasm tests as a process starting a javascript engine with arguments (#240)
    There are several javascript engines (e.g. v8, javascriptcore etc) which wasm tests can run on currently  and this list might be extended. Each of js engine can have (potentially different) set of arguments. e.g.
    `v8 --expose_wasm runtime.js -- --enable-gc --run WasmTestRunner.dll System.Buffers.Tests.dll`
    
    So the idea is to run the wasm runner inside of the wasm app by starting a js engine as a process and pass in respective arguments, in particular test runner and test assembly name.
    
    This change is simplified version of what we want to get in the end.
    
    cc @akoeplinger @steveisok

--- src/Microsoft.DotNet.XHarness.CLI/CommandArguments/WASM/WasmTestCommandArguments.cs
+++ src/Microsoft.DotNet.XHarness.CLI/CommandArguments/WASM/WasmTestCommandArguments.cs
@@ -8,8 +8,57 @@ using Mono.Options;
 
 namespace Microsoft.DotNet.XHarness.CLI.CommandArguments.Wasm
 {
+    /// <summary>
+    /// Specifies a name of a JavaScript engine binary used to run WASM application.
+    /// The invocation of the engine binary doesn't depend on a case,
+    /// i.g. both "v8 ..." and "V8 ..." should work.
+    /// </summary>
+    internal enum JavaScriptEngine
+    {
+        /// <summary>
+        /// V8
+        /// </summary>
+        V8,
+        /// <summary>
+        /// JavaScriptCore
+        /// </summary>
+        JSC,
+        /// <summary>
+        /// SpiderMonkey
+        /// </summary>
+        SM,
+    }
+
     internal class WasmTestCommandArguments : TestCommandArguments
     {
-        protected override OptionSet GetTestCommandOptions() => new OptionSet();
+        private JavaScriptEngine? _engine;
+
+        public JavaScriptEngine Engine
+        {
+            get => _engine ?? throw new ArgumentException("Engine not specified");
+            set => _engine = value;
+        }
+
+        public List<string> EngineArgs { get; set; } = new List<string>();
+
+        public string JSFile { get; set;} = "runtime.js";
+
+        protected override OptionSet GetTestCommandOptions() => new OptionSet{
+            { "engine=|e=", "Specifies the JavaScript engine to be used",
+                v => Engine = ParseArgument<JavaScriptEngine>("engine", v)
+            },
+            { "engine-arg=", "Argument to pass to the JavaScript engine. Can be used more than once.",
+                v => EngineArgs.Add(v)
+            },
+            { "js-file=", "Main JavaScript file to be run on the JavaScript engine. Default is runtime.js",
+                v => JSFile = v
+            },
+        };
+
+        public override void Validate()
+        {
+            base.Validate();
+            Engine = Engine;
+        }
     }
 }
--- src/Microsoft.DotNet.XHarness.CLI/Commands/WASM/WasmTestCommand.cs
+++ src/Microsoft.DotNet.XHarness.CLI/Commands/WASM/WasmTestCommand.cs
@@ -2,32 +2,45 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.DotNet.XHarness.CLI.CommandArguments;
 using Microsoft.DotNet.XHarness.CLI.CommandArguments.Wasm;
 using Microsoft.DotNet.XHarness.Common.CLI;
 using Microsoft.Extensions.Logging;
+using Microsoft.DotNet.XHarness.iOS.Shared.Execution;
+using Microsoft.DotNet.XHarness.iOS.Shared.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Microsoft.DotNet.XHarness.CLI.Commands.Wasm
 {
     internal class WasmTestCommand : TestCommand
     {
-        private const string CommandHelp = "Executes BCL xunit tests on WASM";
+        private const string CommandHelp = "Executes BCL xunit tests on WASM. It starts a JavaScript engine which calls a test runner inside of the WASM application.";
         private readonly WasmTestCommandArguments _arguments = new WasmTestCommandArguments();
         protected override TestCommandArguments TestArguments => _arguments;
-        protected override string CommandUsage { get; } = "wasm test [OPTIONS]";
+        protected override string CommandUsage { get; } = "wasm test [OPTIONS] -- [ENGINE OPTIONS]";
         protected override string CommandDescription { get; } = CommandHelp;
+        public IEnumerable<string> PassThroughArgs => PassThroughArguments;
 
-        protected override Task<ExitCode> InvokeInternal(ILogger logger)
+        protected override async Task<ExitCode> InvokeInternal(ILogger logger)
         {
-            logger.LogDebug($"Wasm Test command called");
+            var processManager = new MacOSProcessManager();
 
-            var result = 0;
-            return Task.FromResult(result == 0 ? ExitCode.SUCCESS : ExitCode.GENERAL_FAILURE);
+            // Expected syntax
+            // xharness wasm test --engine=v8 --engine-arg=--expose_wasm --engine-arg=--some-other-thing --js-file=runtime.js -- --enable-gc --run WasmTestRunner.dll System.Buffers.Tests.dll
+           
+            var result = await processManager.ExecuteCommandAsync(
+                _arguments.Engine.ToString(),
+                _arguments.EngineArgs.Concat(new List<string>{ "--" }).Concat(PassThroughArgs).ToList(),
+                new CallbackLog(m => logger.LogDebug(m)),
+                _arguments.Timeout);
+
+            return result.Succeeded ? ExitCode.SUCCESS : ExitCode.GENERAL_FAILURE;
         }
 
-        public WasmTestCommand() : base(CommandHelp)
+        public WasmTestCommand() : base(CommandHelp, true)
         {
         }
     }
