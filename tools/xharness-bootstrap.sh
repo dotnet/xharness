2a751163ed55f7f5ef3fa27fadba70ea8c725995
Premek Vysoky <premek.vysoky@microsoft.com>
2020-12-10 12:46:27 +0100

    Install xharness to current folder

--- tools/xharness-bootstrap.sh
+++ tools/xharness-bootstrap.sh
@@ -37,15 +37,15 @@ curl -L https://dot.net/v1/dotnet-install.sh -o "$dotnet_install"
 chmod u+x "$dotnet_install"
 dotnet_dir="$here/.dotnet"
 
-printf "Installing dotnet to \033[0;36m%s\033[0m.." "$dotnet_dir"
+printf "Installing dotnet to \033[0;33m%s\033[0m..\n" "$dotnet_dir"
 $dotnet_install --install-dir "$dotnet_dir" --channel "3.1"
 echo 'dotnet installed'
 
 export DOTNET_ROOT="$here/.dotnet"
 
-printf "Installing XHarness.CLI to \033[0;36m./xharness\033[0m.."
-./.dotnet/dotnet tool install --tool-path xharness --version "$xharness_version" --add-source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-eng/nuget/v3/index.json Microsoft.DotNet.XHarness.CLI || true
+printf "Installing XHarness.CLI.."
+./.dotnet/dotnet tool install --tool-path . --version "$xharness_version" --add-source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-eng/nuget/v3/index.json Microsoft.DotNet.XHarness.CLI || true
 echo "XHarness.CLI installed"
 
 echo 'Run XHarness using:'
-printf "\033[0;36m./xharness/xharness help\033[0m\n\n"
+printf "\033[0;33mxharness help\033[0m\n\n"
