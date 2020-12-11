ea857eecd5aab698138fff0eadbbcd552bcaa973
Přemek Vysoký <premek.vysoky@microsoft.com>
2020-12-11 10:52:14 +0100

    Add the XHarness bootstrap script (#396)
    Adds a simple script that installs XHarness locally in a folder so that you can try XHarness out easily.

--- README.md
+++ README.md
@@ -9,13 +9,24 @@ It can locate devices/emulators, install a given application, run it and collect
 It handles application crashes by collecting crash dumps and supports different types of connection modes (Network, USB cable).
 It can output test results in various different formats from text to xUnit/NUnit XML.
 
-## Running the tool
+## System requirements
 
 The tool requires **.NET Core 3.1.201** or later to be run. It is packaged as a `dotnet tool` command and can be installed using the [dotnet tool CLI](https://docs.microsoft.com/en-us/dotnet/core/tools/).
 
 - The iOS scenarios require you to run the tool on MacOS with full Xcode installation
 - Android scenarios are supported on Linux, macOS and Windows systems
 - Browsers scenarios are supported on Linux systems
+
+## Try the tool out quickly
+
+If you want to test the tool quickly (MacOS and Linux only), following script will install the required .NET SDK and the XHarness tool locally in the current folder:
+```
+curl -L https://aka.ms/xharness-bootstrap | bash /dev/stdin
+```
+
+You can delete the folder after you're done, nothing is installed in your system.
+
+## Installation and usage
 
 To install the latest version of the tool run:
 
---
+++ tools/xharness-bootstrap.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+### This script is a quick way to install and test XHarness on any POSIX machine.
+### It installs the .NET Core 3.1 SDK and the XHarness tool, everything in a local folder.
+###
+### Use the following command to run this script from anywhere:
+### curl -L https://aka.ms/xharness-bootstrap | bash /dev/stdin
+
+set -e
+
+version='*'
+
+while [[ $# -gt 0 ]]; do
+    opt="$(echo "$1" | awk '{print tolower($0)}')"
+    case "$opt" in
+      --version)
+        version=$2
+        shift
+        ;;
+      *)
+        echo "Invalid argument: $1"
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+xharness_version="1.0.0-prerelease.$version"
+
+here=$(pwd)
+dotnet_install="$here/dotnet-install.sh"
+
+echo "Getting dotnet-install.sh.."
+curl -L https://dot.net/v1/dotnet-install.sh -o "$dotnet_install"
+chmod u+x "$dotnet_install"
+dotnet_dir="$here/.dotnet"
+
+printf "Installing .NET 3.1 SDK locally to \033[0;33m%s\033[0m..\n" "$dotnet_dir"
+$dotnet_install --install-dir "$dotnet_dir" --channel "3.1"
+echo 'dotnet installed'
+
+export DOTNET_ROOT="$here/.dotnet"
+
+printf "Installing XHarness.CLI locally to \033[0;33m%s\033[0m..\n" "$here"
+./.dotnet/dotnet tool install --tool-path . --version "$xharness_version" --add-source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-eng/nuget/v3/index.json Microsoft.DotNet.XHarness.CLI || true
+echo "XHarness.CLI installed"
+
+echo 'Run XHarness using:'
+printf "\033[0;33mxharness help\033[0m\n\n"
