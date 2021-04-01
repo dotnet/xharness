46098d3853790550751f6321fce19f01edeee818
Přemek Vysoký <premek.vysoky@microsoft.com>
2021-04-01 23:30:14 +0200

    Add `apple device` command (#530)
    Adds a command that searches for a suitable simulator/device and returns its UDID

---
+++ src/Microsoft.DotNet.XHarness.CLI/CommandArguments/Apple/AppleGetDeviceArguments.cs
@@ -0,0 +1,71 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.IO;
+using Microsoft.DotNet.XHarness.Common.CLI;
+using Microsoft.DotNet.XHarness.Common.CLI.CommandArguments;
+using Mono.Options;
+
+namespace Microsoft.DotNet.XHarness.CLI.CommandArguments.Apple
+{
+    internal class AppleGetDeviceArguments : XHarnessCommandArguments
+    {
+        /// <summary>
+        /// Path to where Xcode is located.
+        /// </summary>
+        public string? XcodeRoot { get; set; }
+
+        /// <summary>
+        /// Path to the mlaunch binary.
+        /// Default comes from the NuGet.
+        /// </summary>
+        public string MlaunchPath { get; set; }
+
+        public AppleGetDeviceArguments()
+        {
+            string? pathFromEnv = Environment.GetEnvironmentVariable(EnvironmentVariables.Names.MLAUNCH_PATH);
+            if (!string.IsNullOrEmpty(pathFromEnv))
+            {
+                MlaunchPath = pathFromEnv;
+            }
+            else
+            {
+                // This path is where mlaunch is when the .NET tool is extracted
+                var assemblyPath = Path.GetDirectoryName(System.Reflection.Assembly.GetAssembly(typeof(AppleTestCommandArguments))?.Location);
+                MlaunchPath = Path.Join(assemblyPath, "..", "..", "..", "runtimes", "any", "native", "mlaunch", "bin", "mlaunch");
+            }
+        }
+
+        protected override OptionSet GetCommandOptions() => new()
+        {
+            { "xcode=", "Path where Xcode is installed",
+                v => XcodeRoot = RootPath(v)
+            },
+            { "mlaunch=", "Path to the mlaunch binary",
+                v => MlaunchPath = RootPath(v)
+            },
+        };
+
+        public override void Validate()
+        {
+            if (!File.Exists(MlaunchPath))
+            {
+#if DEBUG
+                string message = $"Failed to find mlaunch at {MlaunchPath}. " +
+                    $"Make sure you specify --mlaunch or set the {EnvironmentVariables.Names.MLAUNCH_PATH} env var. " +
+                    $"See README.md for more information";
+#else
+                string message = $"Failed to find mlaunch at {MlaunchPath}";
+#endif
+                throw new ArgumentException(message);
+            }
+
+            if (XcodeRoot != null && !Directory.Exists(XcodeRoot))
+            {
+                throw new ArgumentException($"Failed to find Xcode root at {XcodeRoot}");
+            }
+        }
+    }
+}
---
+++ src/Microsoft.DotNet.XHarness.CLI/Commands/Apple/AppleGetDeviceCommand.cs
@@ -0,0 +1,116 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.DotNet.XHarness.CLI.CommandArguments.Apple;
+using Microsoft.DotNet.XHarness.Common.CLI;
+using Microsoft.DotNet.XHarness.Common.CLI.CommandArguments;
+using Microsoft.DotNet.XHarness.Common.CLI.Commands;
+using Microsoft.DotNet.XHarness.Common.Logging;
+using Microsoft.DotNet.XHarness.iOS.Shared;
+using Microsoft.DotNet.XHarness.iOS.Shared.Execution;
+using Microsoft.DotNet.XHarness.iOS.Shared.Hardware;
+using Microsoft.DotNet.XHarness.iOS.Shared.Utilities;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.DotNet.XHarness.CLI.Commands.Apple
+{
+    internal class AppleGetDeviceCommand : XHarnessCommand
+    {
+        private readonly AppleGetDeviceArguments _arguments = new();
+
+        protected override XHarnessCommandArguments Arguments => _arguments;
+
+        protected override string CommandUsage { get; } = "apple device [TARGET] [OPTIONS]";
+
+        private const string CommandHelp = "Finds the UDID of a device/simulator for given target";
+        protected override string CommandDescription { get; } = @$"
+{CommandHelp}
+ 
+Arguments:
+";
+
+        public AppleGetDeviceCommand() : base("device", true, CommandHelp)
+        {
+        }
+
+        protected override async Task<ExitCode> InvokeInternal(ILogger logger)
+        {
+            var processManager = new MlaunchProcessManager(_arguments.XcodeRoot, _arguments.MlaunchPath);
+
+            var log = new CallbackLog(m => logger.LogDebug(m));
+            TestTargetOs target;
+
+            try
+            {
+                target = ParseTarget();
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return ExitCode.INVALID_ARGUMENTS;
+            }
+
+            try
+            {
+                if (target.Platform.IsSimulator())
+                {
+                    var simulatorLoader = new SimulatorLoader(processManager);
+
+                    // TODO: Handle companion mode
+                    var (simulator, _) = await simulatorLoader.FindSimulators(target, log, 3, true);
+                    Console.WriteLine(simulator.UDID);
+                }
+                else
+                {
+                    var hardwareDeviceLoader = new HardwareDeviceLoader(processManager);
+                    var device = await hardwareDeviceLoader.FindDevice(target.Platform.ToRunMode(), log, false);
+                    Console.WriteLine(device.UDID);
+                }
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.ToString());
+                return ExitCode.DEVICE_NOT_FOUND;
+            }
+
+            return ExitCode.SUCCESS;
+        }
+
+        private TestTargetOs ParseTarget()
+        {
+            if (ExtraArguments.Count() != 1)
+            {
+                throw CreateException("You have to specify one target platform");
+            }
+
+            string targetName = ExtraArguments.First();
+            TestTargetOs target;
+
+            try
+            {
+                target = targetName.ParseAsAppRunnerTargetOs();
+            }
+            catch (Exception)
+            {
+                throw CreateException($"Failed to parse test target '{targetName}'");
+            }
+
+            if (target.Platform == TestTarget.MacCatalyst)
+            {
+                throw CreateException("Target maccatalyst is not supported for this command");
+            }
+
+            return target;
+        }
+
+        private static Exception CreateException(string message) => new ArgumentException(
+            $"{message}. Available targets are:" +
+            XHarnessCommandArguments.GetAllowedValues(t => t.AsString(), TestTarget.None, TestTarget.MacCatalyst) +
+            Environment.NewLine + Environment.NewLine +
+            "You can also specify desired iOS/tvOS/WatchOS version. Example: ios-simulator-64_13.4");
+    }
+}
