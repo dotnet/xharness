10834d1c47fa073acc9ad09cd90a18249104a26c
Katya Sokolova <esokolov@microsoft.com>
2021-04-12 18:39:25 +0200

    help message polishing

--- src/Microsoft.DotNet.XHarness.CLI/CommandArguments/Android/AndroidTestCommandArguments.cs
+++ src/Microsoft.DotNet.XHarness.CLI/CommandArguments/Android/AndroidTestCommandArguments.cs
@@ -73,7 +73,7 @@ namespace Microsoft.DotNet.XHarness.CLI.CommandArguments.Android
                 }
             },
             {
-                "launch-timeout=|lt=", "Time span in the form of \"00:00:00\" or number of seconds to wait for a device after boot completion",
+                "launch-timeout=|lt=", "Time span in the form of \"00:00:00\" or number of seconds to wait for the device to boot to complete",
                 v =>
                 {
                     if (int.TryParse(v, out var timeout))
