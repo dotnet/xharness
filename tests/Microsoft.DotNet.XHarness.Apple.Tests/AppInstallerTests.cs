8492960e8397d9752118912c7fa3f35c9af43383
Premek Vysoky <premek.vysoky@microsoft.com>
2021-04-22 10:58:28 +0200

    Lower verbosity for app installation

--- tests/Microsoft.DotNet.XHarness.Apple.Tests/AppInstallerTests.cs
+++ tests/Microsoft.DotNet.XHarness.Apple.Tests/AppInstallerTests.cs
@@ -71,6 +71,7 @@ namespace Microsoft.DotNet.XHarness.Apple.Tests
                _mainLog.Object,
                It.IsAny<TimeSpan>(),
                null,
+               It.IsAny<int>(),
                It.IsAny<CancellationToken>()));
         }
 
@@ -92,6 +93,7 @@ namespace Microsoft.DotNet.XHarness.Apple.Tests
                _mainLog.Object,
                It.IsAny<TimeSpan>(),
                null,
+               It.IsAny<int>(),
                It.IsAny<CancellationToken>()));
         }
     }
