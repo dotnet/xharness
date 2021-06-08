0631fd4edbdb124c9c4bcb60485f3939af51151b
Premek Vysoky <premek.vysoky@microsoft.com>
2021-06-08 17:28:21 +0200

    Change SimulatorArguments

---
+++ src/Microsoft.DotNet.XHarness.CLI/CommandArguments/Apple/ArgumentDefinition/ForceInstallationArgument.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.DotNet.XHarness.Common.CLI.CommandArguments;
+
+namespace Microsoft.DotNet.XHarness.CLI.CommandArguments.Apple
+{
+    internal class ForceInstallationArgument : SwitchArgument
+    {
+        public ForceInstallationArgument()
+            : base("force", "Install again even if already installed", false)
+        {
+        }
+    }
+}
