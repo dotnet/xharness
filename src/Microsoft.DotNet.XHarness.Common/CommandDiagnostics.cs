abda8f8df5bff578536e033cad4fc7257821068f
Premek Vysoky <premek.vysoky@microsoft.com>
2021-08-26 14:01:43 +0200

    Implement JSON serialization

--- src/Microsoft.DotNet.XHarness.Common/CommandDiagnostics.cs
+++ src/Microsoft.DotNet.XHarness.Common/CommandDiagnostics.cs
@@ -2,7 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
 using Microsoft.DotNet.XHarness.Common.CLI;
 using Microsoft.Extensions.Logging;
 
@@ -53,9 +56,52 @@ namespace Microsoft.DotNet.XHarness.Common
 
         public void SaveData(string targetFile)
         {
+            _timer.Stop();
+
+            var options = new JsonSerializerOptions
+            {
+#if DEBUG
+                WriteIndented = true,
+#else
+                WriteIndented = false,
+#endif
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+
             try
             {
-                // TODO
+                // Either append current data to the JSON array or create a new file
+                if (File.Exists(targetFile))
+                {
+                    var data = JsonDocument.Parse(File.ReadAllText(targetFile));
+
+                    using var fileStream = new FileStream(targetFile, FileMode.Create, FileAccess.Write);
+                    using var jsonWriter = new Utf8JsonWriter(fileStream);
+
+                    jsonWriter.WriteStartArray();
+
+                    // Copy the existing elements without going into details of what they are
+                    var newData = new List<JsonElement>();
+                    var enumerator = data.RootElement.EnumerateArray();
+                    while (enumerator.MoveNext())
+                    {
+                        enumerator.Current.WriteTo(jsonWriter);
+                    }
+
+                    // New element
+                    JsonSerializer.Serialize(jsonWriter, this, options);
+
+                    jsonWriter.WriteEndArray();
+                }
+                else
+                {
+                    var data = new[]
+                    {
+                        this
+                    };
+                    string json = JsonSerializer.Serialize(data, options);
+                    File.WriteAllText(targetFile, JsonSerializer.Serialize(data, options));
+                }
             }
             catch (Exception e)
             {
