c8b648e002db5380d21ecf24e6a5a67bd870e06b
Premek Vysoky <premek.vysoky@microsoft.com>
2021-08-26 14:54:10 +0200

    Revert "Merge remote-tracking branch '_premun/prvysoky/global-usings' into prvysoky/diagnostics"
    This reverts commit e5634b860c206aeca7d9b83b4afae8d32e126727, reversing
    changes made to d737b9d5abdf2bd92b726cf54698b1c267011f34.
    
    # Conflicts:
    #	src/Microsoft.DotNet.XHarness.CLI/CommandDiagnostics.cs

--- src/Microsoft.DotNet.XHarness.CLI/Program.cs
+++ src/Microsoft.DotNet.XHarness.CLI/Program.cs
@@ -65,9 +65,7 @@ namespace Microsoft.DotNet.XHarness.CLI
 
         public static CommandSet GetXHarnessCommandSet()
         {
-#pragma warning disable IDE0028 // Simplify collection initialization for DEBUG
             var commandSet = new CommandSet("xharness");
-#pragma warning restore IDE0028 // Simplify collection initialization for DEBUG
 
 #if !DEBUG
             if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
