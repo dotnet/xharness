45ff1d8107eba8c008db17e4d633c574c37fa7d7
Premek Vysoky <premek.vysoky@microsoft.com>
2021-09-30 11:53:27 +0200

    Add --adb switch for android state command

--- src/Microsoft.DotNet.XHarness.CLI/CommandArguments/Android/AndroidGetStateCommandArguments.cs
+++ src/Microsoft.DotNet.XHarness.CLI/CommandArguments/Android/AndroidGetStateCommandArguments.cs
@@ -1,14 +1,18 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Microsoft.DotNet.XHarness.CLI.CommandArguments.Android
 {
     internal class AndroidGetStateCommandArguments : XHarnessCommandArguments
     {
-        protected override IEnumerable<Argument> GetArguments() => Enumerable.Empty<Argument>();
+        public ShowAdbPathArgument ShowAdbPath { get; set; } = new();
+
+        protected override IEnumerable<Argument> GetArguments() => new Argument[]
+        {
+            ShowAdbPath
+        };
     }
 }
