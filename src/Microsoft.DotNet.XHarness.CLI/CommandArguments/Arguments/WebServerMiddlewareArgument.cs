e2001d86268d340650fc819070070ba49444d223
Ankit Jain <radical@gmail.com>
2022-03-04 16:38:55 -0500

    [wasm] Add `TypeFromAssemblyArgument` for `assembly,typeName` form of
    .. arguments, and refactor `WebServerMiddlewareArgument` to use that.

--- src/Microsoft.DotNet.XHarness.CLI/CommandArguments/Arguments/WebServerMiddlewareArgument.cs
+++ src/Microsoft.DotNet.XHarness.CLI/CommandArguments/Arguments/WebServerMiddlewareArgument.cs
@@ -3,45 +3,16 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
-using System.IO;
 
 namespace Microsoft.DotNet.XHarness.CLI.CommandArguments;
 
-internal class WebServerMiddlewareArgument : Argument<List<(string path, string type)>>
+internal class WebServerMiddlewareArgument : TypeFromAssemblyArgument<Type>
 {
     public WebServerMiddlewareArgument()
         : base(
               "web-server-middleware=",
               "<path>,<typeName> to assembly and type which contains Kestrel middleware for local test server. Could be used multiple times to load multiple middlewares",
-              new List<(string path, string type)>())
+              repeatable: true)
     {
-    }
-
-    public override void Action(string argumentValue)
-    {
-        var split = argumentValue.Split(',');
-        var file = split[0];
-        var type = split.Length > 1 && !string.IsNullOrWhiteSpace(split[1]) ? split[1] : "GenericHandler";
-
-        Value.Add((file, type));
-    }
-
-    public override void Validate()
-    {
-        base.Validate();
-
-        foreach (var (path, type) in Value)
-        {
-            if (string.IsNullOrWhiteSpace(path))
-            {
-                throw new ArgumentException($"Empty path to middleware assembly");
-            }
-
-            if (!File.Exists(path))
-            {
-                throw new ArgumentException($"Failed to find the middleware assembly at {path}");
-            }
-        }
     }
 }
