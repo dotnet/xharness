949a816a9f41c8cebd8e6e1246f5db80d7286cdc
Premek Vysoky <premek.vysoky@microsoft.com>
2022-03-29 15:22:03 +0200

    Add `apple mlaunch` command

--- src/Microsoft.DotNet.XHarness.CLI/CommandArguments/Apple/AppleMlaunchCommandArguments.cs
+++ src/Microsoft.DotNet.XHarness.CLI/CommandArguments/Apple/AppleMlaunchCommandArguments.cs
@@ -2,22 +2,23 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
 
 namespace Microsoft.DotNet.XHarness.CLI.CommandArguments.Apple;
 
-internal class AppleDeviceCommandArguments : XHarnessCommandArguments, IAppleArguments
+internal class AppleMlaunchCommandArguments : XHarnessCommandArguments, IAppleArguments
 {
-    public DeviceNameArgument DeviceName { get; } = new();
-    public IncludeWirelessArgument IncludeWireless { get; } = new();
-    public XcodeArgument XcodeRoot { get; } = new();
     public MlaunchArgument MlaunchPath { get; } = new();
+    public XcodeArgument XcodeRoot { get; } = new();
+    public TimeoutArgument Timeout { get; set; } = new(TimeSpan.FromMinutes(2));
+    public EnvironmentalVariablesArgument EnvironmentalVariables { get; } = new();
 
     protected override IEnumerable<Argument> GetArguments() => new Argument[]
     {
-        DeviceName,
-        IncludeWireless,
-        XcodeRoot,
         MlaunchPath,
+        XcodeRoot,
+        Timeout,
+        EnvironmentalVariables,
     };
 }
