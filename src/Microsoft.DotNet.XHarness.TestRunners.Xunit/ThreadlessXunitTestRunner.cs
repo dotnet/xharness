ad3a97a4405bdb93439c242c9f6a327f0ffc5be6
pavelsavara <pavel.savara@gmail.com>
2023-12-07 19:08:08 +0100

    - delay 10ms for each test class
    - yield for each test case

--- src/Microsoft.DotNet.XHarness.TestRunners.Xunit/ThreadlessXunitTestRunner.cs
+++ src/Microsoft.DotNet.XHarness.TestRunners.Xunit/ThreadlessXunitTestRunner.cs
@@ -7,12 +7,16 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using Microsoft.DotNet.XHarness.Common;
 using Microsoft.DotNet.XHarness.TestRunners.Common;
 using Xunit;
 using Xunit.Abstractions;
+using Xunit.Sdk;
+using ExceptionUtility = Xunit.ExceptionUtility;
 
 #nullable enable
 namespace Microsoft.DotNet.XHarness.TestRunners.Xunit;
@@ -42,7 +46,7 @@ internal class ThreadlessXunitTestRunner : XunitTestRunnerBase
         foreach (var testAsmInfo in testAssemblies)
         {
             string assemblyFileName = testAsmInfo.FullPath;
-            var controller = new Xunit2(AppDomainSupport.Denied, new NullSourceInformationProvider(), assemblyFileName, configFileName: null, shadowCopy: false, shadowCopyFolder: null, diagnosticMessageSink: diagnosticSink, verifyTestAssemblyExists: false);
+            var controller = new ThreadlessXunit2(AppDomainSupport.Denied, new NullSourceInformationProvider(), assemblyFileName, configFileName: null, shadowCopy: false, shadowCopyFolder: null, diagnosticMessageSink: diagnosticSink, verifyTestAssemblyExists: false);
 
             discoveryOptions.SetSynchronousMessageReporting(true);
             testOptions.SetSynchronousMessageReporting(true);
@@ -171,5 +175,110 @@ internal class ConsoleDiagnosticMessageSink : global::Xunit.Sdk.LongLivedMarshal
         }
 
         return true;
+    }
+}
+
+public class ThreadlessXunitTestMethodRunner : XunitTestMethodRunner
+{
+    public ThreadlessXunitTestMethodRunner(ITestMethod testMethod, IReflectionTypeInfo @class, IReflectionMethodInfo method, IEnumerable<IXunitTestCase> testCases, IMessageSink diagnosticMessageSink, IMessageBus messageBus, ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource, object[] constructorArguments)
+        : base(testMethod, @class, method, testCases, diagnosticMessageSink, messageBus, aggregator, cancellationTokenSource, constructorArguments)
+    {
+    }
+
+    protected override async Task<RunSummary> RunTestCaseAsync(IXunitTestCase testCase)
+    {
+        await Task.Yield();
+        return await base.RunTestCaseAsync(testCase);
+    }
+
+}
+
+public class ThreadlessXunitTestClassRunner : XunitTestClassRunner
+{
+    public ThreadlessXunitTestClassRunner(ITestClass testClass, IReflectionTypeInfo @class, IEnumerable<IXunitTestCase> testCases, IMessageSink diagnosticMessageSink, IMessageBus messageBus, ITestCaseOrderer testCaseOrderer, ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource, IDictionary<Type, object> collectionFixtureMappings)
+        : base(testClass, @class, testCases, diagnosticMessageSink, messageBus, testCaseOrderer, aggregator, cancellationTokenSource, collectionFixtureMappings)
+    {
+    }
+
+    protected override async Task AfterTestClassStartingAsync()
+    {
+        await Task.Delay(10);
+        await base.AfterTestClassStartingAsync();
+    }
+
+    protected override async Task<RunSummary> RunTestMethodAsync(ITestMethod testMethod, IReflectionMethodInfo method, IEnumerable<IXunitTestCase> testCases, object[] constructorArguments)
+    {
+        var runner = new ThreadlessXunitTestMethodRunner(testMethod, Class, method, testCases, DiagnosticMessageSink, MessageBus, new ExceptionAggregator(Aggregator), CancellationTokenSource, constructorArguments);
+        return await runner.RunAsync();
+    }
+}
+
+public class ThreadlessXunitTestCollectionRunner : XunitTestCollectionRunner
+{
+    public ThreadlessXunitTestCollectionRunner(ITestCollection testCollection, IEnumerable<IXunitTestCase> testCases, IMessageSink diagnosticMessageSink, IMessageBus messageBus, ITestCaseOrderer testCaseOrderer, ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource)
+        : base(testCollection, testCases, diagnosticMessageSink, messageBus, testCaseOrderer, aggregator, cancellationTokenSource)
+    {
+    }
+
+    protected override async Task AfterTestCollectionStartingAsync()
+    {
+        await Task.Yield();
+        await base.AfterTestCollectionStartingAsync();
+    }
+
+    protected override async Task<RunSummary> RunTestClassAsync(ITestClass testClass, IReflectionTypeInfo @class, IEnumerable<IXunitTestCase> testCases)
+    {
+        var runner = new ThreadlessXunitTestClassRunner(testClass, @class, testCases, DiagnosticMessageSink, MessageBus, TestCaseOrderer, new ExceptionAggregator(Aggregator), CancellationTokenSource, CollectionFixtureMappings);
+        return await runner.RunAsync();
+    }
+}
+
+public class ThreadlessXunitTestAssemblyRunner : XunitTestAssemblyRunner
+{
+    public ThreadlessXunitTestAssemblyRunner(ITestAssembly testAssembly, IEnumerable<IXunitTestCase> testCases, IMessageSink diagnosticMessageSink, IMessageSink executionMessageSink, ITestFrameworkExecutionOptions executionOptions)
+        : base(testAssembly, testCases, diagnosticMessageSink, executionMessageSink, executionOptions)
+    {
+    }
+
+    protected override async Task AfterTestAssemblyStartingAsync()
+    {
+        await Task.Yield();
+        await base.AfterTestAssemblyStartingAsync();
+    }
+
+    protected override async Task<RunSummary> RunTestCollectionAsync(IMessageBus messageBus, ITestCollection testCollection, IEnumerable<IXunitTestCase> testCases, CancellationTokenSource cancellationTokenSource)
+    {
+        var runner = new ThreadlessXunitTestCollectionRunner(testCollection, testCases, DiagnosticMessageSink, messageBus, TestCaseOrderer, new ExceptionAggregator(Aggregator), cancellationTokenSource);
+        return await runner.RunAsync();
+    }
+}
+
+public class ThreadlessXunitTestFrameworkExecutor : XunitTestFrameworkExecutor
+{
+    public ThreadlessXunitTestFrameworkExecutor(AssemblyName assemblyName, ISourceInformationProvider sourceInformationProvider, IMessageSink diagnosticMessageSink)
+        : base(assemblyName, sourceInformationProvider, diagnosticMessageSink)
+    {
+    }
+
+    protected override async void RunTestCases(IEnumerable<IXunitTestCase> testCases, IMessageSink executionMessageSink, ITestFrameworkExecutionOptions executionOptions)
+    {
+        using var assemblyRunner = new ThreadlessXunitTestAssemblyRunner(TestAssembly, testCases, DiagnosticMessageSink, executionMessageSink, executionOptions);
+        await Task.Delay(10);
+        await assemblyRunner.RunAsync();
+    }
+}
+
+#pragma warning disable xUnit3000 // Classes which cross AppDomain boundaries must derive directly or indirectly from LongLivedMarshalByRefObject
+public class ThreadlessXunit2 : Xunit2, IFrontController, ITestCaseBulkDeserializer
+#pragma warning restore xUnit3000 // Classes which cross AppDomain boundaries must derive directly or indirectly from LongLivedMarshalByRefObject
+{
+    public ThreadlessXunit2(AppDomainSupport appDomainSupport, ISourceInformationProvider sourceInformationProvider, string assemblyFileName, string? configFileName = null, bool shadowCopy = true, string? shadowCopyFolder = null, IMessageSink? diagnosticMessageSink = null, bool verifyTestAssemblyExists = true)
+        : base(appDomainSupport, sourceInformationProvider, assemblyFileName, configFileName, shadowCopy, shadowCopyFolder, diagnosticMessageSink, verifyTestAssemblyExists)
+    {
+        var reField = typeof(Xunit2).GetField("remoteExecutor", BindingFlags.NonPublic | BindingFlags.Instance)!;
+        var an = Assembly.Load(new AssemblyName { Name = Path.GetFileNameWithoutExtension(assemblyFileName) }).GetName();
+        var assemblyName = new AssemblyName { Name = an.Name, Version = an.Version };
+        var replace = new ThreadlessXunitTestFrameworkExecutor(assemblyName, sourceInformationProvider, diagnosticMessageSink!);
+        reField.SetValue(this, replace);
     }
 }
