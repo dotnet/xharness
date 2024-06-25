c1a7044cbe36ea67281412766a417eece02fb3a5
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2024-06-25 11:51:14 +0000

    Prevent infinite loop when closing all tabs before quitting. (#1224)
    * @akoeplinger's feedback
    
    * Update src/Microsoft.DotNet.XHarness.CLI/Commands/WASM/Browser/WasmTestBrowserCommand.cs
    
    Co-authored-by: Alexander Köplinger <alex.koeplinger@outlook.com>
    
    ---------
    
    Co-authored-by: Alexander Köplinger <alex.koeplinger@outlook.com>

--- src/Microsoft.DotNet.XHarness.CLI/Commands/WASM/Browser/WasmTestBrowserCommand.cs
+++ src/Microsoft.DotNet.XHarness.CLI/Commands/WASM/Browser/WasmTestBrowserCommand.cs
@@ -106,8 +106,16 @@ internal class WasmTestBrowserCommand : XHarnessCommand<WasmTestBrowserCommandAr
             // close all tabs before quit is a workaround for broken Selenium - GeckoDriver communication in Firefox
             // https://github.com/dotnet/runtime/issues/101617
             logger.LogInformation($"Closing {driver.WindowHandles.Count} browser tabs before setting the main tab to config page and quitting.");
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(10000);
+
             while (driver.WindowHandles.Count > 1)
             {
+                if (cts.IsCancellationRequested)
+                {
+                    logger.LogInformation($"Timeout while trying to close tabs, {driver.WindowHandles.Count} is left open before quitting.");
+                    break;
+                }
                 driver.Navigate().GoToUrl("about:config");
                 driver.Navigate().GoToUrl("about:blank");
                 driver.Close(); //Close Tab
