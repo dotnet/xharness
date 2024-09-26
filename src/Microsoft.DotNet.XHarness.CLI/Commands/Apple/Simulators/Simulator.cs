447c6b12f216cc6f2868f506e6cab79932dcd0cc
Ivan Povazan <55002338+ivanpovazan@users.noreply.github.com>
2024-09-26 17:01:48 +0200

    Support downloading and installing simulator images with Xcode 16 (#1277) (#1279)

--- src/Microsoft.DotNet.XHarness.CLI/Commands/Apple/Simulators/Simulator.cs
+++ src/Microsoft.DotNet.XHarness.CLI/Commands/Apple/Simulators/Simulator.cs
@@ -9,21 +9,25 @@ namespace Microsoft.DotNet.XHarness.CLI.Commands.Apple.Simulators;
 internal class Simulator
 {
     public string Name { get; }
+    public string Platform { get; }
     public string Identifier { get; }
     public string Version { get; }
-    public string Source { get; }
+    public string? Source { get; }
     public string InstallPrefix { get; }
     public long FileSize { get; }
     public bool IsDmgFormat { get; }
+    public bool IsCryptexDiskImage { get; }
 
-    public Simulator(string name, string identifier, string version, string source, string installPrefix, long fileSize)
+    public Simulator(string name, string platform, string identifier, string version, string? source, string installPrefix, long fileSize, bool isCryptexDiskImage)
     {
         Name = name;
+        Platform = platform;
         Identifier = identifier;
         Version = version;
         Source = source;
         InstallPrefix = installPrefix;
         FileSize = fileSize;
         IsDmgFormat = Identifier.StartsWith("com.apple.dmg.", StringComparison.OrdinalIgnoreCase);
+        IsCryptexDiskImage = isCryptexDiskImage;
     }
 }
---
+++ tests/integration-tests/README.md
@@ -0,0 +1,20 @@
+# Integration tests
+
+This folder includes integration tests projects for different support platforms (iOS, Android, WASM).
+They are used in end-to-end testing scenarios and are referenced from `azure-pipelines-public.yml` E2E templates.
+
+In the relevant `*.proj` files one can configure various setting for execution on Helix like:
+- configuring the Helix queue (e.g., `osx.13.amd64.iphone.open` via `HelixTargetQueue` item group)
+- app bundle to download, send to Helix and test (e.g., `System.Buffers.Tests.app`)
+- etc.
+
+## Testing on scouting queue
+
+NOTE: This is Apple-specific but can be applied to other platforms as well
+
+There are two test projects which can be used on scouting queues which are not used by default:
+
+- Apple/Simulator.Scouting.Tests.proj
+- Apple/Simulator.Scouting.Commands.Tests.proj
+
+When desired, these can be included in the `azure-pipelines-public.yml` so that the CI runs them on a desired scouting queue (check the `HelixTargetQueue` setting) with a particular version of Xcode.
